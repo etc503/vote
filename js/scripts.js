bf73dfcad2938514fe77961b0ebe2a8e440b26ba
Erin Cry <erin.cry@email.com>
2020-07-08 12:52:48 -0700

    changed copy in confirm prompt and now it works

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  const over18 = confirm("Are you over 18? Click OK for yes or Cancel for no.");
+
+  if (over18) {
+    $('#party').show();
+  } else {
+    $('#under-18').show();
+  }
+});
